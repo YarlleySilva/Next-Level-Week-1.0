414fb284e48d022b0c709a95652236f0cbc4d5b1
Yarlley Silva <yarlley.silva@gmail.com>
2020-06-07 17:42:55 -0300

    Projeto Finalizado

---
+++ backend/src/routes.ts
@@ -0,0 +1,47 @@
+import express from "express";
+import { celebrate, Joi } from "celebrate";
+
+import multer from "multer";
+import multerConfig from "./config/multer";
+
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+const routes = express.Router();
+const upload = multer(multerConfig);
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
+
+//lista todos os items
+routes.get("/items", itemsController.index);
+
+//lista um ponto de coleta especifico
+routes.get("/points/:id", pointsController.show);
+
+//lista todos os pontos de coleta por cidade e estado
+routes.get("/points", pointsController.index);
+
+//cria um novo ponto de coleta
+routes.post(
+  "/points",
+  upload.single("image"),
+  celebrate({
+    body: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string().required().email(),
+      whatsapp: Joi.number().required(),
+      latitude: Joi.number().required(),
+      longitude: Joi.number().required(),
+      city: Joi.string().required(),
+      uf: Joi.string().required().max(2),
+      items: Joi.string().required(),
+    })
+  }, {
+    abortEarly: false
+  }),
+  pointsController.create
+);
+
+
+export default routes;
